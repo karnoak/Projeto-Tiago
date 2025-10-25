b7dbbbfbd1a51a1f8a1c7b4fdb0a73e9818520e7
Henry Mikael <dacksoulbr27@gmail.com>
2025-10-25 00:30:09 -0300

    mensage

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Home } from '../pages/home/home';
+import { Navbar } from "../components/navbar/navbar";
+import { Footer } from "../components/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Home],
+  imports: [RouterOutlet, Home, Navbar, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
